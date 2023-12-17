f08562030b082d2dd83da6ba7f82fd4ba3049977
Kufre Akpan <contactkufreakpan@gmail.com>
2023-12-17 22:50:36 +0000

    updated

--- factors.c
+++ factors.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <inttypes.h>
+#include <gmp.h>
 #include <time.h>
 
 typedef struct {
-    uint64_t factor1;
-    uint64_t factor2;
+    mpz_t factor1;
+    mpz_t factor2;
 } Factorization;
 
-Factorization factorize(uint64_t num) {
-    Factorization result = {0, 0};
-    for (uint64_t i = 2; i <= sqrt(num); ++i) {
-        if (num % i == 0) {
-            result.factor1 = i;
-            result.factor2 = num / i;
-            return result;
+void factorize(mpz_t num, Factorization *result) {
+    mpz_t sqrt_num;
+    mpz_init(sqrt_num);
+    mpz_sqrt(sqrt_num, num);
+
+    mpz_set_ui(result->factor1, 0);
+    mpz_set_ui(result->factor2, 0);
+
+    mpz_t i;
+    mpz_init(i);
+
+    for (mpz_set_ui(i, 2); mpz_cmp(i, sqrt_num) <= 0; mpz_add_ui(i, i, 1)) {
+        if (mpz_divisible_p(num, i)) {
+            mpz_set(result->factor1, i);
+            mpz_divexact(result->factor2, num, i);
+            return;
         }
     }
-    return result;
+
+    mpz_clear(sqrt_num);
+    mpz_clear(i);
 }
 
 int main(int argc, char *argv[]) {
@@ -38,21 +48,34 @@ int main(int argc, char *argv[]) {
     char buffer[256];
     time_t start_time = time(NULL);
 
-    while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        uint64_t num = strtoull(buffer, NULL, 10);
-        Factorization factors = factorize(num);
+    Factorization factors;
+    mpz_init(factors.factor1);
+    mpz_init(factors.factor2);
 
-        if (factors.factor1 != 0) {
-            printf("%" PRIu64 "=%" PRIu64 "*%" PRIu64 "\n", num, factors.factor1, factors.factor2);
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        mpz_t num;
+        mpz_init_set_str(num, buffer, 10);
+
+        factorize(num, &factors);
+
+        if (mpz_cmp_ui(factors.factor1, 0) != 0) {
+            gmp_printf("%Zd=%Zd*%Zd\n", num, factors.factor1, factors.factor2);
         }
 
         if (time(NULL) - start_time > 5) {
             printf("Time limit exceeded\n");
+            mpz_clear(num);
+            mpz_clear(factors.factor1);
+            mpz_clear(factors.factor2);
             fclose(file);
             exit(EXIT_FAILURE);
         }
+
+        mpz_clear(num);
     }
 
+    mpz_clear(factors.factor1);
+    mpz_clear(factors.factor2);
     fclose(file);
     return EXIT_SUCCESS;
 }
